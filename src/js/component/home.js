3390e985a31d01b4d89222a47d22be115b656a52
analiabelo98 <analiabelo.42@gmail.com>
2021-04-29 13:29:20 +0000

    traffic light

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 export function Home() {
+	const [color, setcolor] = useState();
+
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="flex-container">
+			<div className=" p-4">
+				<button
+					onClick={() => setcolor("red")}
+					className="btn btn-danger">
+					Red
+				</button>
+				<button
+					onClick={() => setcolor("yellow")}
+					className="btn btn-warning">
+					Yellow
+				</button>
+				<button
+					onClick={() => setcolor("green")}
+					className="btn btn-success">
+					Green
+				</button>
+			</div>
+			<div className=" container col-4">
+				<div className="row">
+					<div className="d-flex flex-column col-12 ">
+						<div
+							className={
+								"circle " +
+								"my-3 " +
+								(color == "red" ? color : "")
+							}></div>
+						<div
+							className={
+								"circle " +
+								"my-3 " +
+								(color == "yellow" ? color : "")
+							}></div>
+						<div
+							className={
+								"circle " +
+								"my-3 " +
+								(color == "green" ? color : "")
+							}></div>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 }
